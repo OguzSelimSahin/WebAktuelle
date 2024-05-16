873a673983b0447a1555c72f63400658533ee000
ErayZor <erayzot045@gmail.com>
2024-05-16 18:02:28 +0200

    ci test

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'js'],
-  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-  testTimeout: 10000, 
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: '.', outputName: 'junit.xml' }]
+  ]
 };
